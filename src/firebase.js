4325c05d6ee610cadf5f8e83d3eea606635cdb63
PsmD <tbvjeld7@naver.com>
2021-12-09 22:38:32 +0900

    Set up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ9yoNgLp_emuddxNbllkgY9O-thReI6A",
+  authDomain: "dwitter-996bb.firebaseapp.com",
+  projectId: "dwitter-996bb",
+  storageBucket: "dwitter-996bb.appspot.com",
+  messagingSenderId: "887648846355",
+  appId: "1:887648846355:web:5403ed3600221fdfa975e6"
+};
+
+export default firebase.initializeApp(firebaseConfig);
