1de57ef9a0a14ae5fa338d933169ab063d52350b
PsmD <tbvjeld7@naver.com>
2021-12-10 23:47:15 +0900

    Router set up

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ9yoNgLp_emuddxNbllkgY9O-thReI6A",
-  authDomain: "dwitter-996bb.firebaseapp.com",
-  projectId: "dwitter-996bb",
-  storageBucket: "dwitter-996bb.appspot.com",
-  messagingSenderId: "887648846355",
-  appId: "1:887648846355:web:5403ed3600221fdfa975e6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
