66003b4adb0af08546fd8db5cf2419deee227b6e
PsmD <tbvjeld7@naver.com>
2021-12-17 15:05:40 +0900

    Dweeting #4 ..Preview images upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Dweet from "components/Dweet";
 const Home = ({userObj}) => {
   const [dweet, setDweet] = useState("");
   const [dweets, setDweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   useEffect(() => {
     dbService.collection("dweets").onSnapshot((snapshot) => {
       const dweetArray = snapshot.docs.map((doc) => ({
@@ -29,6 +30,21 @@ const Home = ({userObj}) => {
     } = event;
     setDweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -39,7 +55,14 @@ const Home = ({userObj}) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Dweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {dweets.map((dweet) => (
