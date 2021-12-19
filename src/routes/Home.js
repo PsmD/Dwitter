c936e259e7e558a96dc11d53fa1025447cc8a21b
PsmD <tbvjeld7@naver.com>
2021-12-19 21:26:19 +0900

    Cleaning code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "fbase";
+import { dbService } from "fbase";
 import Dweet from "components/Dweet";
+import DweetFactory from "components/DweetFactory";
 
 const Home = ({userObj}) => {
-  const [dweet, setDweet] = useState("");
   const [dweets, setDweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     dbService.collection("dweets").onSnapshot((snapshot) => {
       const dweetArray = snapshot.docs.map((doc) => ({
@@ -16,66 +14,9 @@ const Home = ({userObj}) => {
       setDweets(dweetArray);
     });
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const dweetObj = {
-      text: dweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-    await dbService.collection("dweets").add(dweetObj);
-    setDweet("");
-    setAttachment("");
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setDweet(value);
-  };
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachment = () => setAttachment(null);
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={dweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Dweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <div>
+      <DweetFactory userObj={userObj} />
       <div>
         {dweets.map((dweet) => (
           <Dweet
